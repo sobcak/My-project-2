0dbdd62cee4ff753c914a64c1974d3e0c0fdb0b8
apates <papousekgames@gmail.com>
2026-09-11 17:39:48 +0200

    map shit

--- Assets/scripts/MapScript.cs
+++ Assets/scripts/MapScript.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 public class MapScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int MapWidth, MapHeight;
+    [SerializeField] private GameObject Tile;
+
+
+    private void Awake()
     {
-        
+        StartGeneration();
     }
 
-    // Update is called once per frame
-    void Update()
+    void StartGeneration()
     {
-        
+        for(int x = MapWidth; x >= 0; x++)
+        {
+            for(int y = MapHeight; x >= 0; y++)
+            {
+                float OffsetX = (x + y) / 2;
+                float OffsetY = (x - y) / 4;
+                GameObject PlacedTile = Instantiate(Tile, new Vector3(OffsetX, OffsetY, 0), Quaternion.identity);
+            }
+        }
     }
+
 }
