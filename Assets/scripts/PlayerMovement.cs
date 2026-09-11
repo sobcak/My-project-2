9e44f8a3dbab927457fc6bb50df627e99ad1d320
EuWolfie <adam.skrna@icloud.com>
2026-09-11 20:41:23 +0200

    fuck you oliver

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
         Vector2 direction = new Vector2(xInput, yInput).normalized;
-        body.velocity = direction * speed;
+        body.linearVelocity = direction * speed;
 
  }
 }
