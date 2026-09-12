7ec1391c1f42ca9cd541b8172cae9f6a8ac8e0f9
sobcak <omax@sent.com>
2026-09-12 21:16:41 +0200

    kredity. Ne ty v imperriu

--- Assets/scripts/bettermenu.cs
+++ Assets/scripts/bettermenu.cs
@@ -4,10 +4,25 @@ using UnityEngine.SceneManagement;
 public class bettermenu : MonoBehaviour
 {
     [SerializeField] string JmenoSceny;
+    [SerializeField] private GameObject sklo;
 
+    // teleports to scene in JmenoSceny
     public void YouShallMove()
     {
         SceneManager.LoadScene(JmenoSceny);
         Debug.Log($"Teleporting to {JmenoSceny}");
         
-    }}
+    }
+    // show/hide
+    public void YouShallMaybeSee()
+    {
+        if(sklo.activeInHierarchy)
+        {
+            sklo.SetActive(false);
+        }
+        else
+        {
+            sklo.SetActive(true);
+        }
+    }
+}
