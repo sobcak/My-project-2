65398fa760d4870c5ec9548d3ead395a76af3edb
EuWolfie <adam.skrna@icloud.com>
2026-09-12 02:29:08 +0200

    Working Human Manager with Triger
    I will shoot myself, dont remove my trigerbox pleas

--- Assets/scripts/DataStorage.cs
+++ Assets/scripts/DataStorage.cs
@@ -6,4 +6,9 @@ public class DataStorage : ScriptableObject
     public int NumberOfHumans;
     public int AvailableFood;
     public int AvailableHousing;
+    public int Workers;
+    
+    public int Stone;
+    public int Wood;
+    public int brick;
 }
