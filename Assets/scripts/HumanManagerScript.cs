426ff6a59fbd5f99470371ccdcb7fe2a2462b6ac
EuWolfie <adam.skrna@icloud.com>
2026-09-13 01:19:35 +0200

    BirthRate Increese

--- Assets/scripts/HumanManagerScript.cs
+++ Assets/scripts/HumanManagerScript.cs
@@ -95,8 +95,8 @@ public class HumanManagerScript : MonoBehaviour
             int pairs = DataStorage.Instance.Workers / 2;  // workers are adults kind of 
             for (int i = 0; i < pairs; i++)
             {
-                int kid = Random.Range(0, 6);
-                if (kid == 5)
+                int kid = Random.Range(0, 4);
+                if (kid == 3)
                 {
                     CreateHuman();
                 }
