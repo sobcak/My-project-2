b6bc02970ad8f271fe308a6a058d92865c77c90d
EuWolfie <adam.skrna@icloud.com>
2026-09-13 13:23:02 +0200

    FixedStuff

--- Assets/scripts/DataStorage.cs
+++ Assets/scripts/DataStorage.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class DataStorage : ScriptableObject
 {
     // DEBUG
-    public bool EraDebug = true;
+    public bool EraDebug = false; // Debug set true only when debuging or it will break the fucking game !!!!!!!!!!!!!!!!!!!!!!!
     
     
     public int MaxResources { get; set; } = 50;
