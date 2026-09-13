396f9ea1595a7cb6d39b385aabbf6980b0c87200
apates <papousekgames@gmail.com>
2026-09-13 08:04:43 +0200

    buildin

--- Assets/scripts/BuildingManager.cs
+++ Assets/scripts/BuildingManager.cs
@@ -34,56 +34,49 @@ public class BuildingManager : MonoBehaviour
     public void GetToWork()
     {
         int bonusScaling = CalculateWorkMultiplier(DataStorage.Instance.CurrentEra);
-        Debug.Log($"Got To Work");
-        
+
         foreach (Building b in buildings)
         {
-            Debug.Log("Testy");
-            if (b.CurrentWorkforce <= 0) continue; // Skip buildings without workers
+            if (b.CurrentWorkforce <= 0) continue; //skip building without wokres
 
             int requiredWood = b.MaterialToRun.Wood;
             int requiredStone = b.MaterialToRun.Stone;
             int requiredBrick = b.MaterialToRun.Bricks;
 
-            // Check if there are enough resources to run production
-            if (DataStorage.Instance.Wood >= requiredWood && 
-                DataStorage.Instance.Stone >= requiredStone && 
+            //verify you have enought materials
+            if (DataStorage.Instance.Wood >= requiredWood &&
+                DataStorage.Instance.Stone >= requiredStone &&
                 DataStorage.Instance.brick >= requiredBrick)
             {
-                // Deduct inputs
+                //deduct inputs
                 DataStorage.Instance.Wood -= requiredWood;
                 DataStorage.Instance.Stone -= requiredStone;
                 DataStorage.Instance.brick -= requiredBrick;
 
-                // Add production output scaled by workforce and era
-                
-                Debug.Log("Here");
-
+                //add production outputs 
                 int woodToAdd = (int)Math.Floor((double)b.MaterialProduction.Wood * b.CurrentWorkforce / b.DesiredWorkforce * bonusScaling);
                 DataStorage.Instance.Wood += woodToAdd;
+
                 int stoneToAdd = (int)Math.Floor((double)b.MaterialProduction.Stone * b.CurrentWorkforce / b.DesiredWorkforce * bonusScaling);
                 DataStorage.Instance.Stone += stoneToAdd;
-                int brickToAdd = (int)Math.Floor((double)2 * b.CurrentWorkforce / b.DesiredWorkforce * bonusScaling); //there is a constant 2 for the time being i will fix it in the morning it works this way
-               /* Debug.Log(brickToAdd.ToString() +"hereous");
-                Debug.Log(b.MaterialProduction.Bricks+"bricky");
-                Debug.Log(b.CurrentWorkforce + "   hereous2" + b.MaterialProduction.Bricks);*/
+
+                //smelter shitty code
+                int brickToAdd = (int)Math.Floor((double)b.MaterialProduction.Bricks * b.CurrentWorkforce / b.DesiredWorkforce * bonusScaling);
                 DataStorage.Instance.brick += brickToAdd;
-                Debug.Log(DataStorage.Instance.brick + " brick after");
 
                 int foodToAdd = b.MaterialProduction.Food * b.CurrentWorkforce * bonusScaling;
                 DataStorage.Instance.AvailableFood += foodToAdd;
-                
+
                 int toolsToAdd = b.MaterialProduction.Tools * b.CurrentWorkforce * bonusScaling;
                 DataStorage.Instance.Tools += toolsToAdd;
+
                 int furnitureToAdd = b.MaterialProduction.Furniture * b.CurrentWorkforce * bonusScaling;
                 DataStorage.Instance.Furniture += furnitureToAdd;
-                Debug.Log(DataStorage.Instance.Wood + " wood after");
-
             }
         }
     }
-    
-    
+
+
     // fuck 
 
     static bool TryToBuild(Building b)
