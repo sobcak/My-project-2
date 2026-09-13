09cc9fdd3011e7126b525a4be766e9077bea473b
EuWolfie <adam.skrna@icloud.com>
2026-09-13 10:38:33 +0200

    Commit Suicide

--- Assets/scripts/DataStorage.cs
+++ Assets/scripts/DataStorage.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class DataStorage : ScriptableObject
 {
     // DEBUG
-    public bool EraDebug = false;
+    public bool EraDebug = true;
     
     
     public int MaxResources { get; set; } = 50;
