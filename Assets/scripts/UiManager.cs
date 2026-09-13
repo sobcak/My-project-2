7c033e191911a78bb4385422b792bb667a019aef
sobcak <omax@sent.com>
2026-09-13 13:32:59 +0200

    ui fixes part 5

--- Assets/scripts/UiManager.cs
+++ Assets/scripts/UiManager.cs
@@ -12,6 +12,8 @@ public class UiManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI Stonetext;
     [SerializeField] private TextMeshProUGUI Woodtext;
     [SerializeField] private TextMeshProUGUI Bricktext;
+    [SerializeField] private TextMeshProUGUI Tooltext;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,5 +31,7 @@ public class UiManager : MonoBehaviour
         Stonetext.text = $"Stone: {dataStorage.Stone.ToString()}";
         Woodtext.text = $"Wood: {dataStorage.Wood.ToString()}";
         Bricktext.text = $"Bricks: {dataStorage.brick.ToString()}";
+        Tooltext.text = $"Tools: {dataStorage.Tools.ToString()}";
+
     }
 }
